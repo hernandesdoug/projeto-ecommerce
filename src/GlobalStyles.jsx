a8aeb6e8a66d847e125d8e9616543241a079378d
Douglas Hernandes <douglashernandes@icloud.com>
2025-10-09 18:23:21 -0300

    inicializando projeto

---
+++ src/GlobalStyles.jsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+/* reset css */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Bebas Neue", sans-serif;
+}    
+`;
+
+export default GlobalStyles;
---
+++ src/routes.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Carrinho from "./pages/Carrinho";
+import Pedidos from "./pages/Pedidos";
+import Produtos from "./pages/Produtos";
+import Usuarios from "./pages/Usuarios";
+
+function AppRoutes() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Produtos />} />
+          <Route path="/carrinho/:id" element={<Carrinho />} />
+          <Route path="/pedidos/:id" element={<Pedidos />} />
+          <Route path="/usuarios/" element={<Usuarios />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+  
+  export default AppRoutes;
