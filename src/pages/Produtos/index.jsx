f07718d9dbc145823148e51316faa28a412f50e1
Douglas Hernandes <douglashernandes@icloud.com>
2025-10-15 14:03:41 -0300

    instalando dependencias

--- src/pages/Produtos/index.jsx
+++ src/pages/Produtos/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import api from "../../services/api";
+
 
 function Produtos() {
 
