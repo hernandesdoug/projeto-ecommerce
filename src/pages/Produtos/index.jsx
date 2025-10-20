e3bbd4b6b750174912d27bddbb5a61ac11efe00e
Douglas Hernandes <douglashernandes@icloud.com>
2025-10-20 06:40:18 -0300

    incluindo imports

--- src/pages/Produtos/index.jsx
+++ src/pages/Produtos/index.jsx
@@ -1,25 +1,18 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import api from "../../services/api";
-
 
 function Produtos() {
 
     return (
         <>
         <div>
-            <header>
-                <h1>Eletronicos</h1>
-                <nav>
-                    <input type="text" placeholder='pesquise um produto...' />
-                </nav>
-            </header>
+           
             <div>
-
+            
             </div>
-            <footer>
-
-            </footer>
+        
         </div>
         </>
     )
